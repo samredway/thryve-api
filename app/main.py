c504469956601547a0fffadd61499477acbd8f72
Sam Redway <sam.redway-consultant@turntown.digital>
2024-06-03 18:43:27 +0100

    update requirements and add cors for local

--- app/main.py
+++ app/main.py
@@ -1,18 +1,32 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.plaid_manager import PlaidManager
 from app.schema import GetPlaidLinkTokenResponse
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 plaid_manager = PlaidManager()
 
 
-@app.get("/")
+@app.get('/')
 def read_root() -> str:
     return "Server up!"
 
 
-@app.get("/plaid-link-token")
+@app.get('/plaid-link-token')
 def read_plaid_link_token() -> GetPlaidLinkTokenResponse:
     link_token = plaid_manager.get_link_token()
     return GetPlaidLinkTokenResponse(plaid_link_token=link_token)
